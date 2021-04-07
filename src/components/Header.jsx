5ad4660584da0a44893b6348e9a982de37f4c5a2
gerry <gerry.baird@uk.ibm.com>
2021-04-07 10:31:27 +0100

    Removed grid from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import {
   AppBar,
   Toolbar,
-  Grid,
   Typography,
-  InputBase,
   IconButton,
   makeStyles,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
-import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -26,31 +26,27 @@ export default function Header() {
   const classes = useStyles();
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Grid container alignItems="center">
-          <Grid item>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-          </Grid>
-          <Grid item sm>
-            <Typography variant="h6" className={classes.title}>
-              Sales Invoice Entry
-            </Typography>
-          </Grid>
-          <Grid item>
-            <IconButton>
-              <PowerSettingsNewIcon fontSize="small" color="secondary" />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h6" className={classes.title}>
+            Sales Invoice Entry
+          </Typography>
+
+          <IconButton>
+            <PowerSettingsNewIcon fontSize="small" color="secondary" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
