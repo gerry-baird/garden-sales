a2d00763951d1e13529d8141d5cd26d87ffc0dd4
gerry <gerry.baird@uk.ibm.com>
2021-04-07 14:30:40 +0100

    Basic Form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import {
   makeStyles,
   CssBaseline,
+  Paper,
   createMuiTheme,
   ThemeProvider,
 } from "@material-ui/core";
 
 import Header from "./components/Header";
+import InvForm from "./components/InvForm";
 
 const theme = createMuiTheme({});
 
+const useStyles = makeStyles((theme) => ({
+  pageContent: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div>
       <Header />
+      <Paper className={classes.pageContent}>
+        <InvForm />
+      </Paper>
       <CssBaseline />
     </div>
   );
